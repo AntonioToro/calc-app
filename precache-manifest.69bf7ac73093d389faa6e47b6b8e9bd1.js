6c6fd019a15fbb16df06bad49eab004ef1e5a311
AntonioToro <antoniotorolucas@gmail.com>
2023-01-04 23:56:50 +0100

    Updates

---
+++ precache-manifest.69bf7ac73093d389faa6e47b6b8e9bd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db8a0cbd20f14533997a",
+    "url": "/atjl-calc-app/static/css/main.c368f4c7.chunk.css"
+  },
+  {
+    "revision": "db8a0cbd20f14533997a",
+    "url": "/atjl-calc-app/static/js/main.fb7b2462.chunk.js"
+  },
+  {
+    "revision": "01cecc11447887d527a6",
+    "url": "/atjl-calc-app/static/js/runtime~main.e59f5f49.js"
+  },
+  {
+    "revision": "2c1f21c4014525acc8ce",
+    "url": "/atjl-calc-app/static/js/2.bfc623bc.chunk.js"
+  },
+  {
+    "revision": "707de353a21ca18632aa863d4a54bb43",
+    "url": "/atjl-calc-app/index.html"
+  }
+];
