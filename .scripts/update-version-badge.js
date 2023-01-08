fab3d8e121b161f57e87d0d26ba50fd32694752d
AntonioToro <antoniotorolucas@gmail.com>
2023-01-08 17:14:52 +0100

    feat(Scripts): :hammer: create new script update-version-badge.js

--- .scripts/update-version-badge.js
+++ .scripts/update-version-badge.js
@@ -1,8 +1,15 @@
+const pc = require("picocolors")
 const replace = require("replace")
 const {version} = require('../package.json')
 
-replace({
-    regex: /version-\d+(\.\d+)+-blue/g,
-    replacement: `version-${version}-blue`,
-    paths: ['README.md'],
-})
+try {
+    // Localizamos en el archivo Readme.md con un regex el badge de la versión de la app y lo remplazamos por la nueva.
+    replace({
+        regex: /version-\d+(\.\d+)+-blue/g,
+        replacement: `version-${version}-blue`,
+        paths: ['README.md'],
+    })
+    console.log(`✔ update badge to version ${pc.bold(version)} in ${pc.bold('README.md')}`)
+} catch {
+    console.log(`✖ Error to update badge to version ${pc.bold(version)} in ${pc.bold('README.md')}`)
+}
